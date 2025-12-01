216bbc1ba80b4eea41058b7fce7b6228841e2834
victor <vic2792@gmail.com>
2025-12-01 10:41:22 +0100

    tema 4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("elige una opcion");
+            System.out.println("1. Sumar 1+1");
+            System.out.println("2 restar 2-1");
+            System.out.println("3 salir");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            if (option == 1) {
+                //int n1 = 4;
+                //int n2 = 3;
+                System.out.println("la suma es " + (1 + 1));
+            } else if (option == 2) {
+                System.out.println("la restas es " + (2-1));
+            } else if (option == 3) {
+                System.out.println("saliendo");
+            } else {
+                System.out.println("opcion incorrecta");
+            }
+        }while (option != 3);
         }
     }
 }
